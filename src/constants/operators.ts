79ed476e61a514e18a535ba9e4be8de141cd3b98
Tunde Abolaji <exqeesolutions@gmail.com>
2022-02-11 22:03:06 +0100

    Add basic operators

---
+++ src/constants/operators.ts
@@ -0,0 +1,4 @@
+export const ADDITION: string = "+";
+export const SUBTRACTION: string = "-";
+export const MULTIPLICATION: string = "*";
+export const DIVISION: string = "/";
---
+++ src/operators.ts
@@ -0,0 +1,16 @@
+import { ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION } from "./constants";
+
+export const operators = {
+  [ADDITION]: function (x: number, y: number): number {
+    return x + y;
+  },
+  [SUBTRACTION]: function (x: number, y: number): number {
+    return x - y;
+  },
+  [MULTIPLICATION]: function (x: number, y: number): number {
+    return x * y;
+  },
+  [DIVISION]: function (x: number, y: number): number {
+    return x / y;
+  },
+};
