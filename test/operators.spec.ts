e18e7e5aea38ea92d7aeea9268debf9e3c9caf54
Tunde Abolaji <exqeesolutions@gmail.com>
2022-02-12 14:35:47 +0100

    Write test cases for operators

--- test/operators.spec.ts
+++ test/operators.spec.ts
@@ -4,9 +4,37 @@
 
 import { expect } from "chai";
 import { operators } from "../src/lib";
+import {
+  ADDITION,
+  SUBTRACTION,
+  MULTIPLICATION,
+  DIVISION,
+} from "../src/constants";
+
+const FUNCTION = "function";
 
 describe("Operators", () => {
   it("Should be an object", () => {
     expect(operators).to.be.an("object");
+  });
+
+  it("Should have a `+` method that adds up two or more numbers", () => {
+    expect(operators).to.have.property(ADDITION).that.is.a(FUNCTION);
+    expect(operators[ADDITION](2, 2)).to.equal(4);
+  });
+
+  it("Should have a `-` method that subtracts", () => {
+    expect(operators).to.have.property(SUBTRACTION).that.is.a(FUNCTION);
+    expect(operators[SUBTRACTION](4, 2)).to.equal(2);
+  });
+
+  it("Should have a `*` method that muiltiplies two or more numbers", () => {
+    expect(operators).to.have.property(MULTIPLICATION).that.is.a(FUNCTION);
+    expect(operators[MULTIPLICATION](2, 2)).to.equal(4);
+  });
+
+  it("Should have a `/` method that divides two or more numbers", () => {
+    expect(operators).to.have.property(DIVISION).that.is.a(FUNCTION);
+    expect(operators[DIVISION](10, 5)).to.equal(2);
   });
 });
